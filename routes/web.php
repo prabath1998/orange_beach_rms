50e4dd1a98b4332f33936ab919a6d5481657ab3a
prabath1998 <prabhathudayanga1998@gmail.com>
2023-01-20 21:45:05 +0530

    controllers & main admin layouts added

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,14 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\MenuController;
+use App\Http\Controllers\Admin\ReservationController;
+use App\Http\Controllers\Admin\TableController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -35,10 +30,10 @@ Route::middleware('auth')->group(function () {
 
 Route::middleware(['auth', 'admin'])->name('admin.')->prefix('admin')->group(function () {
     Route::get('/', [AdminController::class, 'index'])->name('index');
-    // Route::resource('/categories', CategoryController::class);
-    // Route::resource('/menus', MenuController::class);
-    // Route::resource('/tables', TableController::class);
-    // Route::resource('/reservations', ReservationController::class);
+    Route::resource('/categories', CategoryController::class);
+    Route::resource('/menus', MenuController::class);
+    Route::resource('/tables', TableController::class);
+    Route::resource('/reservations', ReservationController::class);
 });
 
 require __DIR__.'/auth.php';
